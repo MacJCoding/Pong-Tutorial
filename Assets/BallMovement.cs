1c885b148b5ddc9f66f124dff546c3016a20e26e
MacJCoding <mjcoding99@gmail.com>
2022-12-30 11:42:02 -0500

    Initial commit

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+	bool gameStarted = false;
+	bool isGoingRight = true;
+	public bool isGoingUp = true;
+	
+    void FixedUpdate()
+    {
+        if(!gameStarted && Input.GetKey(KeyCode.Space))
+		{
+			gameStarted = true;
+		}
+		if(gameStarted)
+		{
+			if(isGoingRight)
+			{
+				transform.Translate(0.1f,0f,0f);
+			} else {
+				transform.Translate(-0.1f,0f,0f);
+			}
+			if(isGoingUp)
+			{
+				transform.Translate(0f,0.1f,0f);
+			} else {
+				transform.Translate(0f,-0.1f,0f);
+			}
+		}
+		if(transform.position.x > 10 || transform.position.x < -10)
+		{
+			gameStarted = false;
+			transform.position = new Vector3(0f,0f,0f);
+		}
+    }
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Panel")
+		{
+			isGoingRight = !isGoingRight;
+		}
+		if(col.gameObject.tag == "Boundary")
+		{
+			isGoingUp = !isGoingUp;
+		}
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public bool isPlayerOne = true;
+	
+    void FixedUpdate()
+    {
+		if(isPlayerOne && Input.GetKey(KeyCode.W) && transform.position.y < 3)
+		{
+			transform.Translate(0f, 0.1f, 0f);
+		}
+		if(isPlayerOne && Input.GetKey(KeyCode.S) && transform.position.y > -3)
+		{
+			transform.Translate(0f, -0.1f, 0f);
+		}
+		if(!isPlayerOne && Input.GetKey(KeyCode.UpArrow) && transform.position.y < 3)
+		{
+			transform.Translate(0f, 0.1f, 0f);
+		}
+		if(!isPlayerOne && Input.GetKey(KeyCode.DownArrow) && transform.position.y > -3)
+		{
+			transform.Translate(0f, -0.1f, 0f);
+		}
+    }
+}
